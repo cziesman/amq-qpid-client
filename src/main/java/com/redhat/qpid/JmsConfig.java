ba1a21f7fed951161cefb2395ac814f12fa5aeae
Craig Ziesman <craig.ziesman@gmail.com>
2021-11-18 12:34:29 -0700

    Added cached connections

--- src/main/java/com/redhat/qpid/JmsConfig.java
+++ src/main/java/com/redhat/qpid/JmsConfig.java
@@ -1,12 +1,14 @@
 package com.redhat.qpid;
 
 import org.apache.qpid.jms.JmsConnectionFactory;
-import org.messaginghub.pooled.jms.JmsPoolConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -43,17 +45,39 @@ public class JmsConfig {
     private String verifyHostName;
 
     @Bean
-    public JmsPoolConnectionFactory jmsPoolConnectionFactory() {
+    public JmsTemplate jmsTemplate(CachingConnectionFactory connectionFactory) {
 
-        JmsPoolConnectionFactory pooledConnectionFactory = new JmsPoolConnectionFactory();
-        pooledConnectionFactory.setConnectionFactory(jmsConnectionFactory());
-        pooledConnectionFactory.setMaxConnections(brokerMaxConnections);
-        pooledConnectionFactory.setUseProviderJMSContext(true);
+        JmsTemplate jmsTemplate = new JmsTemplate();
+        jmsTemplate.setConnectionFactory(connectionFactory);
 
-        return pooledConnectionFactory;
+        jmsTemplate.afterPropertiesSet();
+
+        return jmsTemplate;
     }
 
-    protected JmsConnectionFactory jmsConnectionFactory() {
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(CachingConnectionFactory connectionFactory) {
+
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setCacheLevel(25);
+
+        return factory;
+    }
+
+    @Bean
+    public CachingConnectionFactory cachingConnectionFactory(JmsConnectionFactory connectionFactory) {
+
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+        cachingConnectionFactory.setSessionCacheSize(brokerMaxConnections);
+        cachingConnectionFactory.setTargetConnectionFactory(connectionFactory);
+        cachingConnectionFactory.afterPropertiesSet();
+
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public JmsConnectionFactory jmsConnectionFactory() {
 
         JmsConnectionFactory factory = new JmsConnectionFactory();
         factory.setRemoteURI(remoteUri());
@@ -76,6 +100,8 @@ public class JmsConfig {
 
         LOG.debug(uriComponents.toUriString());
 
-        return uriComponents.toUriString();
+        LOG.debug(uriComponents.toUriString());
+
+        return String.format("failover:(%s)", uriComponents.toUriString());
     }
 }
